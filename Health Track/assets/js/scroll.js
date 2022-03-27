7f0eb5287d704049e4eddc961dd8b94f063c3c29
rsastrecapelozza <94131094+rsastrecapelozza@users.noreply.github.com>
2022-03-27 01:05:01 -0300

    Delete Health Track directory

--- Health Track/assets/js/scroll.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('.header-top');
-    header.classList.toggle('active-scroll', window.scrollY > 1)
-})
--- Health Track/assets/js/scrollb.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('.header-top-b');
-    header.classList.toggle('active-scroll-b', window.scrollY > 1)
-})
-
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('.header-aar');
-    header.classList.toggle('active-scroll-b', window.scrollY > 1)
-})
